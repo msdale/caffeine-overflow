25978c5b0231a8f57abd4c9fa5bf9d19d7f23c72
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2021-11-28 15:12:10 -0500

    only thing altered is to link the two html pages

---
+++ assets/js/list.js
@@ -0,0 +1,15 @@
+//js for shopping list page
+
+// var targetList = "Target: []";
+
+// //show shopping list for target
+// var targetViewList = function () {
+//     var targetView = JSON.parse(window.localStorage.getItem("highscores")) || [];
+//         if(targetView !== null) {
+//             targetList = targetView;
+//         };
+// }
+
+//click retailer to show shopping list
+// $(document).on("click", ".target", function name that creates this call);
+// $(document).on("click", ".walmart", function name that creates this call);
